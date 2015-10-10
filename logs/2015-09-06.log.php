5c2ec3bdfaae97a616acc671e1a1f7c42dccaf36
Ondřej Fibich <ondrej.fibich@gmail.com>
2015-10-10 20:59:04 +0200

    Removes log files.

--- logs/2015-09-06.log.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2015-09-06 21:38:03 -- error: Nezachycená Exception: Zvýšení databáze selháno:  v souboru application/libraries/MY_Controller.php na řádce 456   URL: cs/login
-
-2015-09-06 21:45:01 -- error: Nezachycená Kohana_Exception: Invalid validation rule used: valid_number v souboru application/libraries/forge/Form_Input.php na řádce 650   URL: cs/settings/users
-
--- logs/2015-09-12.log.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2015-09-12 18:57:59 -- error: Nezachycená Exception: Zvýšení databáze selháno: Upgrade DB: 1.2.0~alpha9 failedCause: SQL query failed: ALTER TABLE `users` ADD `password_is_onetime` TINYINT( 1 ) NOT NULL AFTER `password_request`  v souboru application/libraries/MY_Controller.php na řádce 467   URL: cs/login
-

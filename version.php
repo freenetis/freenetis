3a3b98ff8be2168f4a3e30fa364cec208ac672f9
Ondřej Fibich <fibich@freenetis.org>
2016-05-24 22:48:04 +0200

    Release 1.1.18

--- version.php
+++ version.php
@@ -15,4 +15,4 @@
  * The current version of FreenetIS.
  * This value must be changed by developers in a new release of FreenetIS.
  */
-define('FREENETIS_VERSION', '1.1.17');
+define('FREENETIS_VERSION', '1.1.18');

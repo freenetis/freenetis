44b9036b88eec17ef6098deb7862cc457555c6a6
Ondřej Fibich <fibich@freenetis.org>
2018-06-20 23:58:13 +0200

    Rewrite of test DEB package check script for /bin/sh

--- tests/deb_package_check.sh
+++ tests/deb_package_check.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 MODULE=freenetis
 VERSION=`cat version.php | grep "FREENETIS_VERSION" | cut -d"'" -f 4`
 DEB_PREFIX=deb_packages/freenetis_${VERSION}
-DEBIANS=(lenny squeeze wheezy jessie)
+DEBIANS="lenny squeeze wheezy jessie"
 
 cd application/vendors/deb/freenetis
-mkdir ../deb_packages || true # fix in source
+mkdir ../deb_packages 2>/dev/null || true # fix in source
 
-for OS in ${DEBIANS[*]}
+echo "$DEBIANS" | tr " " "\n" | while read OS
 do
     echo "Debianization for $VERSION $OS"
     ./debianization.sh "$VERSION" "$OS"
